eda7429a769c819a02167b40893736b67006d877
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2024-03-02 19:48:59 +0530

    minor adjustment

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const connectToDb = async () => {
             return;
         }
 
-        const db = await mongoose.connect(process.env.DATABASE_URL);
+        const db = await mongoose.connect(process.env.MONGODB_URI);
         connection.isConnected = db.connections[0].readyState;
     } catch (error) {
         console.log(error);
